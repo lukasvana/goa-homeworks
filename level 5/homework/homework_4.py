1b9923a781ae41834d3643f3ffd7cdbaf23f3cf2
lukasvana <lukasvanadze916@gmail.comgit>
2024-07-08 21:20:55 +0400

    second commit

---
+++ level 5/homework/homework_4.py
@@ -0,0 +1,4 @@
+print("goa is the best academy in georgia")
+#ეს არის სწორი იმიტომ რომ სტრინგის სახითაა და არის ასევე ბრჩხილებში მოცემული
+print(goa is the best academy in georgia)
+#ეს არის არასწორი იმიტომ რომ არ არის სტრინგის სახით/აკლია ბრჭყალები გარშემო
---
+++ level 5/homework/homewrok.py
@@ -0,0 +1,7 @@
+print(hello!) #ეს არის სწორი რადგან არ არის სიტყვა სტრინგის სახით/ბრჭყალებში არ არის
+age = input "how old are you:" #ეს არის არასწორი რადგან აკლია ბრჩხილები
+print 13 #ეს არის არასწორი რადაგან აკლია ბრჩხილები
+print(13)
+name = (input("whats your name: "))
+print(name)
+print("hello!")
